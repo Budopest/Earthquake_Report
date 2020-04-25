13c67acff5cfc30312c600c0278c59f8ce83c239
budopest <abdo.mohamed5111995@gmail.com>
2020-04-25 19:36:51 +0200

    Earthquake class attributes created

--- app/src/main/java/com/funprojects/earthquakereport/Earthquake.java
+++ app/src/main/java/com/funprojects/earthquakereport/Earthquake.java
@@ -1,10 +1,20 @@
 package com.funprojects.earthquakereport;
 
-public class Earthquake {
-    private String location;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public Earthquake(String location){
+public class Earthquake {
+
+    private int magnitude;
+    private long time;
+    private String location;
+    private String url;
+
+    public Earthquake(int magnitude,long time,String location,String url){
         this.location = location;
+        this.magnitude = magnitude;
+        this.time = time;
+        this.url = url;
     }
 
     public String getLocation() {
@@ -13,5 +23,40 @@ public class Earthquake {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public int getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(int magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getDisplayedDate(){
+        Date date = new Date(getTime());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return simpleDateFormat.format(date);
+
+    }
+    public String getDisplayedTime(){
+        Date date = new Date(getTime());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:mm a");
+        return simpleDateFormat.format(date);
     }
 }
