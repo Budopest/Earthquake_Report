748aaf8ffe1b766431f2b1e0bc6d26b1450c30ea
budopest <abdo.mohamed5111995@gmail.com>
2020-05-04 04:56:49 +0200

    Loading bar and connectivity warning

--- app/src/main/java/com/funprojects/earthquakereport/MainActivity.java
+++ app/src/main/java/com/funprojects/earthquakereport/MainActivity.java
@@ -7,11 +7,14 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final int EARTHQUAKE_LOADER_ID = 1;
     private EarthquakeAdapter earthquakeAdapter;
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +31,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
          earthquakeAdapter = new EarthquakeAdapter(this,new ArrayList<Earthquake>());
         ListView listView = (ListView) findViewById(R.id.quakelist);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        listView.setEmptyView(mEmptyStateTextView);
         listView.setAdapter(earthquakeAdapter);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(this.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
 
         LoaderManager loaderManager = getSupportLoaderManager();
         loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        }
+        else mEmptyStateTextView.setText("No internet connection");
 
     }
 
@@ -42,6 +59,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(@NonNull Loader<ArrayList<Earthquake>> loader, ArrayList<Earthquake> earthquakes) {
+
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.no_earthquakes);
+
         // Clear the adapter of previous earthquake data
         earthquakeAdapter.clear();
 
@@ -55,6 +79,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(@NonNull Loader<ArrayList<Earthquake>> loader) {
-
+        earthquakeAdapter.clear();
     }
 }
